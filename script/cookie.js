96e8702c7ac02010d742ae14c2e4d78535f3b09d
Anthony CASANAVE <anthony.casanave@gmail.com>
2018-12-03 16:24:58 +0100

    Cookies working, display working, credit++ working

--- script/cookie.js
+++ script/cookie.js
@@ -1,132 +1,85 @@
+let addCreditButton = document.getElementById("cible");
+let creditCount = 0;
+let cliqueCredits = document.getElementById("cliqueCredits");
+cliqueCredits = 0;
+document.getElementById("cliqueAddMoney").textContent = 0;
+
 //Gestion automatique des cookies.
-//function createCookie(name, value, days) {
-//    if (days) {
-//        var date = new Date();
-//        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-//        var expires = "; expires=" + date.toGMTString();
-//    } else var expires = "";
-//    document.cookie = name + "=" + value + expires + "; path=/cookies";
-//    console.log()
-//}
-//
-//function readCookie(name) {
-//    var nameEQ = name + "=";
-//    var ca = document.cookie.split(';');
-//    for (var i = 0; i < ca.length; i++) {
-//        var c = ca[i];
-//        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-//        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-//    }
-//    return meatCount;
-//}
-//
-//function eraseCookie(name) {
-//    createCookie(name, "", -1);
-//}
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/cookies";
+    console.log()
+}
 
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    //    return meatCount;
+}
 
-//$(function(){
-//function setCookie(cname, cvalue, exdays) {
-//    var d = new Date();
-//    d.setTime(d.getTime() + (exdays24*60*60*1000));
-//    var expires = "expires="+ d.toUTCString();
-//    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-//}
-// function getCookie(cname) {
-//    var name = cname + "=";
-//    var decodedCookie = decodeURIComponent(document.cookie);
-//    var ca = decodedCookie.split(';');
-//    for(var i = 0; i <ca.length; i++) {
-//        var c = ca[i];
-//        while (c.charAt(0) == ' ') {
-//            c = c.substring(1);
-//        }
-//        if (c.indexOf(name) == 0) {
-//            return c.substring(name.length, c.length);
-//        }
-//    }
-//    return false;
-//}
-//if(!getCookie('moneyTotal')){
-//    let nbViande=0;
-//}
-//else{
-//    nbViande=getCookie('moneyTotal');
-//}
-//$('#cible').click(function(){
-//    nbViande++;
-//    setCookie('moneyTotal',nbViande,7);
-//    console.log(getCookie('moneyTotal'));
-//});
-//    
-//    let addMeatButton = document.getElementById("cible");
-//
-//})
-//cliqueAddMoney
-
-
-
-//
-//let creditCount = document.getElementById("cliqueAddMoney").textContent;
-//
-////Chargement des cookies à l'ouverture de la page.
-//var readCreditCount = getCookie("moneyTotal");
-//
-//if (readMeatCount != null) {
-//    creditCount = readCreditCount;
-//}
-//else{
-//    creditCount = 0;
-//    createCookie("numberOfClicks", creditCount, 7);
-//}
-
-
-
-//Truc
-
-
-
-document.getElementById("cliqueCredits").textContent = creditCount;
-
-addMeatButton.onclick = function () {
-    creditCount++;
-    
-    createCookie("numberOfClicks", creditCount, 7);
-
-    document.getElementById("cliqueCredits").textContent = creditCount;
-    console.log(readCreditCount);
-
+function eraseCookie(name) {
+    createCookie(name, "", -1);
 }
 
 
+$(function () {
+
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return false;
+    }
+    if (!getCookie('moneyTotal')) {
+        creditCount = 0;
+    } else {
+        creditCount = readCookie('moneyTotal');
+    }
+    $('#cible').click(function () {
+
+        creditCount++;
+        setCookie('moneyTotal', creditCount, 7);
+        
+        console.log(getCookie('moneyTotal'));
+         document.getElementById("cliqueAddMoney").textContent = creditCount;
+    });
+
+})
 
 
 
 
+//Chargement des cookies à l'ouverture de la page.
+var readCreditCount = readCookie("moneyTotal");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (readCreditCount != null) {
+    document.getElementById("cliqueAddMoney").textContent = readCreditCount;
+} else {
+    document.getElementById("cliqueAddMoney").textContent = 0;
+    createCookie("numberOfClicks", creditCount, 7);
+}
