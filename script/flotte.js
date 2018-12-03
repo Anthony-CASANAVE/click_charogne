171f940f68f31db7208959c68183ceb5ec34e319
Vacance <kenny.vacance@telecom-st-etienne.fr>
2018-12-03 09:06:42 +0100

    Start flotte.js

---
+++ script/flotte.js
@@ -0,0 +1,18 @@
+let chasseX1 = document.getElementById("chasseur");
+let chasseX5 = document.getElementById("chasseurX5");
+let chasseX10 = document.getElementById("chasseurX10");
+
+let totalChasse = document.getElementById("chasseurNombre");
+
+$(function() {
+    
+    $('#chasseur').onclick(function(event ) {
+        if ($(''))
+        
+    })
+    
+    
+    
+    
+    
+})
