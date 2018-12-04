ef4971f5e19271ae868816fe7a7afa34398c076d
Anthony CASANAVE <anthony.casanave@gmail.com>
2018-12-04 11:02:07 +0100

    Cookies, text and function working and stable

--- script/cookie.js
+++ script/cookie.js
@@ -1,37 +1,41 @@
-let addCreditButton = $("#cible");
-let creditCount = 0;
-let cliqueCredits = $("#cliqueCredits");
-cliqueCredits = 0;
-$("#cliqueAddMoney").val = 0;
-
-//Gestion automatique des cookies.
-function createCookie(name, value, days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        var expires = "; expires=" + date.toGMTString();
-    } else var expires = "";
-    document.cookie = name + "=" + value + expires + "; path=/cookies";
-    console.log()
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    //    return meatCount;
-}
-
-function eraseCookie(name) {
-    createCookie(name, "", -1);
-}
-
+/*jslint browser: true*/ /*global  $*/
 
 $(function () {
+
+
+    let addCreditButton = $("#cible");
+    let creditCount = 0;
+    let cliqueCredits = $("#cliqueCredits");
+    cliqueCredits = 0;
+    $("#cliqueAddMoney").val(0);
+
+    //Gestion automatique des cookies.
+    function createCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        } else var expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/cookies";
+        console.log()
+    }
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        //    return meatCount;
+    }
+
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    }
+
+
 
     function setCookie(cname, cvalue, exdays) {
         var d = new Date();
@@ -64,22 +68,33 @@ $(function () {
 
         creditCount++;
         setCookie('moneyTotal', creditCount, 7);
-        
+
         console.log(getCookie('moneyTotal'));
-         $("#cliqueAddMoney").text(creditCount);
+        $("#cliqueAddMoney").text(creditCount);
     });
 
+
+
+
+
+
+    //Chargement des cookies à l'ouverture de la page.
+    var readCreditCount = readCookie("moneyTotal");
+
+    if (readCreditCount != null) {
+        $("#cliqueAddMoney").text(readCreditCount);
+    } else {
+        $("#cliqueAddMoney").text(0);
+        createCookie("numberOfClicks", creditCount, 7);
+    }
+
+
+
+
+
+
+
+
+
+    //    Fin du jquery !!!!
 })
-
-
-
-
-//Chargement des cookies à l'ouverture de la page.
-var readCreditCount = readCookie("moneyTotal");
-
-if (readCreditCount != null) {
-    $("#cliqueAddMoney").text(readCreditCount);
-} else {
-    $("#cliqueAddMoney").text(0);
-    createCookie("numberOfClicks", creditCount, 7);
-}
