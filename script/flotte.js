95e5543422577d7a787738ff06d41d9e3ff88988
Vacance <kenny.vacance@telecom-st-etienne.fr>
2018-12-04 11:38:01 +0100

    .js: .js: .js:

--- script/flotte.js
+++ script/flotte.js
@@ -1,19 +1,34 @@
 $(function () {
 
 
-    let chasseX1 = document.getElementById("chasseur");
-    let chasseX5 = document.getElementById("chasseurX5");
-    let chasseX10 = document.getElementById("chasseurX10");
-    
-    let disabe
+    $("#cible").click(function () {
+        
+//        $('#chasseur').removeClass('disabled');
+//        alert($("#cliqueAddMoney").text());
+        if ($("#cliqueAddMoney").text() >= 5) {
 
-    let totalChasse = document.getElementById("chasseurNombre");
+            $("#chasseur").removeClass("disabled");
+
+        };
+    });
+
+
+    //    let chasseX1 = $("#chasseur").click(function () {
+    //
+    //        if ($('#selecteur').val() == 0) {
+    //
+    //        }
+    //
+    //    });
+    //
+    //    let chasseX5 = $("#chasseurX5").click();
+    //    let chasseX10 = $("#chasseurX10").click();
+    //
+    //    let totalChasse = $("#chasseurNombre").val();
 
 
 
 
-
- 
 
 
 
